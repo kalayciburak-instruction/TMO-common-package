4ea9b023b6ecc8d5774cdd2e94f77d70d6020c75
Burak <kalayciburak1996@gmail.com>
2023-06-03 01:12:38 +0300

    add cors configuration

--- src/main/java/com/kodlamaio/commonpackage/configuration/security/SecurityConfig.java
+++ src/main/java/com/kodlamaio/commonpackage/configuration/security/SecurityConfig.java
@@ -7,16 +7,32 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        var configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5431")); // Only allow requests from this origin
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        var source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         var converter = new JwtAuthenticationConverter();
         converter.setJwtGrantedAuthoritiesConverter(new KeycloakJwtRoleConverter());
 
-        http.authorizeHttpRequests()
+        http.cors().and().authorizeHttpRequests()
                 .requestMatchers("/api/cars/check-car-available/**", "/api/customers", "/api/payments/check", "/actuator/**")
                 .permitAll()
                 .requestMatchers("/api/**")
